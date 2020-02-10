9fe389640db388c55ed636168e4e497d902fac6d
Ahmed Besbes <ahmed.besbes@hotmail.com>
2020-02-10 22:32:17 +0100

    log shape

--- models.py
+++ models.py
@@ -6,25 +6,23 @@ from torchvision.models import resnet34
 
 class FaceNetModel(nn.Module):
     def __init__(self, embedding_size, num_classes, pretrained=False):
-        super (FaceNetModel, self).__init__()
-        
-        self.model            = resnet34(pretrained)
-        self.embedding_size   = embedding_size
-        self.model.fc         = nn.Linear(2048*3*3, self.embedding_size)
+        super(FaceNetModel, self).__init__()
+
+        self.model = resnet34(pretrained)
+        self.embedding_size = embedding_size
+        self.model.fc = nn.Linear(2048*3*3, self.embedding_size)
         self.model.classifier = nn.Linear(self.embedding_size, num_classes)
-    
-    
+
     def l2_norm(self, input):
         input_size = input.size()
-        buffer     = torch.pow(input, 2)
-        normp      = torch.sum(buffer, 1).add_(1e-10)
-        norm       = torch.sqrt(normp)
-        _output    = torch.div(input, norm.view(-1, 1).expand_as(input))
-        output     = _output.view(input_size)
-    
+        buffer = torch.pow(input, 2)
+        normp = torch.sum(buffer, 1).add_(1e-10)
+        norm = torch.sqrt(normp)
+        _output = torch.div(input, norm.view(-1, 1).expand_as(input))
+        output = _output.view(input_size)
+
         return output
-    
-    
+
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
@@ -35,18 +33,18 @@ class FaceNetModel(nn.Module):
         x = self.model.layer3(x)
         x = self.model.layer4(x)
         x = x.view(x.size(0), -1)
+        print(x.shape)
         x = self.model.fc(x)
-        
+
         self.features = self.l2_norm(x)
         # Multiply by alpha = 10 as suggested in https://arxiv.org/pdf/1703.09507.pdf
-        alpha         = 10
+        alpha = 10
         self.features = self.features*alpha
-        
+
         return self.features
-    
-    
+
     def forward_classifier(self, x):
         features = self.forward(x)
-        res      = self.model.classifier(features)
-        
+        res = self.model.classifier(features)
+
         return res
